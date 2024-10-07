5b22eb12f7d1234a6038090c7dbbd684ac84207c
Hamza-ALyemeni <hamzaalchamak442979@gmail.com>
2024-10-07 08:54:04 +0300

    fetching meals

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
-
+import { fetchAppMeals } from './http.js'
 
 function App() {
+  const [meals , setMeals] = useState([]);
+  const[error , setError] = useState();
+
+  useEffect(()=>{
+    async function fetchMeals() {
+      try {
+        const data = await fetchAppMeals();
+        console.log(data);
+        setMeals(data);
+      } catch (error) {
+        setError({message : error.message || 'Failed to fetch user places'});
+      }
+    }
+
+    fetchMeals();
+  },[]);
   return (
     <>
       <Header />
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <div id="meals">
+        {meals.map((meal) => {
+          return (
+            <div className="meal-item" key={meal.id}>
+              <article >
+                <img src={`http://localhost:3000/${meal.image}`} alt="" />
+                <h3>{meal.name}</h3>
+                <div className="meal-item-price">${meal.price}</div>
+                <p className=".meal-item-description">{meal.description}</p>
+                <button className="button">add to cart</button>
+              </article> 
+            </div>
+          )
+        })}
+      </div>
     </>
   );
 }
